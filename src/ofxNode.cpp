a385987a0d956d16af4ff2f8edb375081ed9ad4f
Timothy Scaffidi <timothy.scaffidi@gmail.com>
2013-03-27 21:03:59 -0400

    Initial revision

---
+++ src/ofxNode.cpp
@@ -0,0 +1,145 @@
+//
+//  ofxNode.cpp
+//  ofxNode
+//
+//  Created by Timothy Scaffidi on 3/24/13.
+//
+//
+
+#include "ofxNode.h"
+
+ofxNode::ofxNode() :
+ofNode(),
+content(NULL),
+drawOffset(0,0,0),
+color(255,255,255,255) {
+    
+}
+
+ofxNode::~ofxNode() {
+    delete content;
+    ofNode::~ofNode();
+}
+
+void ofxNode::setContent(ofBaseDraws * content, bool centerContent) {
+    this->content = content;
+    if(centerContent && content != NULL) {
+        drawOffset.x = content->getWidth()*-0.5;
+        drawOffset.y = content->getHeight()*-0.5;
+    }
+}
+
+void ofxNode::customDraw() {
+    if(content != NULL) {
+        ofPushStyle();
+        ofSetColor(color);
+        content->draw(drawOffset);
+        ofPopStyle();
+    }
+}
+
+
+ofxNodeObject::ofxNodeObject() {
+    
+}
+
+ofxNodeObject::~ofxNodeObject() {
+    for (int i=0; i<components.size(); i++) {
+        delete components[i];
+    }
+}
+
+void ofxNodeObject::draw() {
+    for (int i=0; i<components.size(); i++) {
+        if(components[i]->getVisibility()){
+            components[i]->draw();
+        }
+    }
+}
+
+void ofxNodeObject::loadXML(string filename) {
+    ofxXmlSettings xml(filename);
+    
+    loadNode(xml, NULL);
+}
+
+void ofxNodeObject::addComponent(ofxNode * component){
+    components.push_back(component);
+}
+
+void ofxNodeObject::loadNode(ofxXmlSettings &xml, ofxNode * parent){
+    int numNodes = xml.getNumTags("node");
+    
+    
+    ofLogVerbose() << "ofxNodeObject::loadNode() numNodes = " << numNodes;
+    
+    ofxNode * curNode;
+    for(int i=0;i<numNodes;i++){
+        if(xml.pushTag("node", i)){
+            ofVec3f pos, scale(1.0);
+            ofVec4f orientation;
+            
+            if(xml.pushTag("position")){
+                pos.x = xml.getValue("x", 0.0);
+                pos.y = xml.getValue("y", 0.0);
+                pos.z = xml.getValue("z", 0.0);
+                xml.popTag();
+            }
+            
+            if(xml.pushTag("orientation")){
+                orientation.x = xml.getValue("x", 0.0);
+                orientation.y = xml.getValue("y", 0.0);
+                orientation.z = xml.getValue("z", 0.0);
+                orientation.w = xml.getValue("w", 0.0);
+                xml.popTag();
+            }
+            
+            if(xml.pushTag("scale")){
+                scale.x = xml.getValue("x", 1.0);
+                scale.y = xml.getValue("y", 1.0);
+                scale.z = xml.getValue("z", 1.0);
+                xml.popTag();
+            }
+            
+            curNode = new ofxNode();
+            curNode->setParent(*parent);
+            curNode->setPosition(pos);
+            curNode->setOrientation(ofQuaternion(orientation));
+            curNode->setScale(scale);
+            
+            if(xml.pushTag("content")){
+                curNode->setContent(loadContent(xml));
+                xml.popTag();
+            }
+            
+            addComponent(curNode);
+            
+            if(xml.pushTag("children")){
+                loadNode(xml, curNode);
+                xml.popTag();
+            }
+            
+            xml.popTag(); //node
+        }
+    }
+}
+
+ofBaseDraws * ofxNodeObject::loadContent(ofxXmlSettings &xml) {
+    ofBaseDraws * content = NULL;
+    
+    string type = xml.getValue("type", "");
+    ofLogVerbose() << "ofxNodeObject::loadContent() type = " << type;
+
+    if(type == "texture") {
+        ofTexture * tex = new ofTexture();
+        string path = xml.getValue("path", "");
+        ofLogVerbose() << "ofxNodeObject::loadContent() path = " << path;
+        ofLoadImage(*tex, path);
+        content = tex;
+    }
+    
+    return content;
+}
+
+
+
---
+++ src/ofxNode.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxXmlSettings.h"
+
+//
+//  ofxNode.h
+//  ofxNode
+//
+//  Created by Timothy Scaffidi on 3/24/13.
+//
+//
+
+class ofxNode : public ofNode {
+public:
+    ofxNode();
+    ~ofxNode();
+    void customDraw();
+    void setContent(ofBaseDraws * content, bool centerContent = true);
+    ofBaseDraws * getContent() { return content; }
+    void setDrawOffset(ofPoint drawOffset) { this->drawOffset = drawOffset; }
+    
+    void setVisibility(bool v) { visible = v; }
+    void setColor(ofColor c) { color = c; }
+    
+    bool getVisibility() { return visible; }
+    ofColor getColor() { return color; }
+    
+private:
+    ofBaseDraws* content;
+    ofPoint drawOffset;
+    ofColor color;
+    bool visible;
+};
+
+class ofxNodeObject {
+public:
+    ofxNodeObject();
+    ~ofxNodeObject();
+    void loadXML(string filename);
+    void addComponent(ofxNode * component);
+    void draw();
+    void draw(float x, float y) { ofPushMatrix(); ofTranslate(x, y); draw(); ofPopMatrix(); }
+    void draw(ofPoint p) { draw(p.x,p.y); }
+private:
+    vector<ofxNode *> components;
+    void loadNode(ofxXmlSettings &xml, ofxNode * parent);
+    ofBaseDraws * loadContent(ofxXmlSettings &xml);
+};
